ad7fbd603dbb5919bae250c6efcc3bfd4a284771
cata <cata@e4d843f8-3f35-4d48-9620-962c376a76f2>
2013-01-10 14:07:01 +0000

    Optimize bitCount.

--- bitmanip.h
+++ bitmanip.h
@@ -3,12 +3,15 @@
 #include "defines.h"
 
 /* Returns the number of set bits */
-inline byte popCount(u64 x) {
-  return __builtin_popcountll(x);
+inline int popCount(u64 x) {
+  x -= (x >> 1) & 0x5555555555555555;
+  x = (x & 0x3333333333333333) + ((x >> 2) & 0x3333333333333333);
+  x = (x + (x >> 4)) & 0x0f0f0f0f0f0f0f0f;
+  return (x * 0x0101010101010101) >> 56;
 }
 
 /* Returns the index of the last set bit. When x only has one set bit, this works as a logarithm. */
-inline byte ctz(u64 x) {
+inline int ctz(u64 x) {
   return __builtin_ctzll(x);
 }
 
